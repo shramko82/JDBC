015839e850507769658eef37a1d0c21cd7aa858f
shramko82 <shramko82@gmail.com>
2015-02-24 22:29:49 +0200

    JDBC
    Prepared example of using JDBC with MySQL table

--- src/JDBCProgram.java
+++ src/JDBCProgram.java
@@ -28,16 +28,10 @@ public class JDBCProgram {
 		choice = JOptionPane
 				.showInputDialog(
 						null,
-						"1. Create Employees Table\n"
-								+ "2. Create Products Table\n"
-								+ "3. Insert data into Employees Table\n"
-								+ "4. Insert data into Orders Table\n"
-								+ "5. Retrieve data for Employees Table\n"
-								+ "6. Retrieve data for Orders Table\n"
-								+ "7. Update Employees Table\n"
-								+ "8. Update Employees Table Using a Prepared Statement\n"
-								+ "9. Update many records of Orders Table Using a Prepared Statement\n"
-								+ "10. List the name of employees who bought CD'sn"
+						"1. Create Users Table\n"
+								+ "2. Add data into Users Table\n"
+								+ "3. Retrieve data from Users Table\n"
+								+ "4. Retrieve data from Users Table by Name\n"
 								+ "0. Exit\n\n" + "Enter your choice");
 		if (choice == null)
 			System.exit(0);
@@ -49,34 +43,16 @@ public class JDBCProgram {
 
 	public static void getSelected(int choice) {
 		if (choice == 1) {
-			createEmployees();
+			createUsers();
 		}
 		if (choice == 2) {
-			createOrders();
+			addUser();
 		}
 		if (choice == 3) {
-			insertEmployees();
+			retrieveUsers();
 		}
 		if (choice == 4) {
-			insertOrders();
-		}
-		if (choice == 5) {
-			retrieveEmployees();
-		}
-		if (choice == 6) {
-			retrieveOrders();
-		}
-		if (choice == 7) {
-			updateEmployees();
-		}
-		if (choice == 8) {
-			updateEmployeesPrepared();
-		}
-		if (choice == 9) {
-			updateOrdersPrepared();
-		}
-		if (choice == 10) {
-			dynamicQuery();
+			retrieveUsersWithStatement();
 		}
 	}
 
@@ -100,122 +76,67 @@ public class JDBCProgram {
 		return con;
 	}
 
-	public static void createEmployees() {
+	public static void createUsers() {
 		Connection con = getConnection();
 
 		String createString;
-		createString = "create table Employees ("
+		createString = "create table Users ("
 				+ "ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, "
-				+ "Name VARCHAR(30) NOT NULL)";
+				+ "Name VARCHAR(30) NOT NULL, "
+				+ "LastName VARCHAR(30), "
+				+ "Date_Birth Date)";
 		try {
 			stmt = con.createStatement();
 			stmt.executeUpdate(createString);
 			stmt.close();
 			con.close();
-			JOptionPane.showMessageDialog(null, "Employees Table Created");
+			JOptionPane.showMessageDialog(null, "Users Table Created");
 		} catch (SQLException ex) {
 			System.err.println("SQLException: " + ex.getMessage());
 		}
 
 	}
 
-	/*
-	 * CREATE TABLE Orders ( Prod_ID INTEGER, ProductName VARCHAR(20),
-	 * Employee_ID INTEGER );
-	 */
-
-	public static void createOrders() {
+	public static void addUser() {
 		Connection con = getConnection();
 
-		String createString;
-		createString = "create table Orders ("
-				+ "ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, "
-				+ "ProductName VARCHAR(20) NOT NULL, "
-				+ "Employee_ID INTEGER )";
+		String n1 = JOptionPane.showInputDialog(null, "Insert Name");
+		String n2 = JOptionPane.showInputDialog(null, "Insert Last Name");
+		String n3 = JOptionPane.showInputDialog(null, "Insert Date of birth (YYYY-MM-DD)");
+		
+
+		String insertString = "insert into users (Name,LastName,Date_Birth) "
+				+ "values ('"+n1+"','"+n2+"','"+n3+"')";
 
 		try {
 			stmt = con.createStatement();
-			stmt.executeUpdate(createString);
-
-			stmt.close();
-			con.close();
-			JOptionPane.showMessageDialog(null, "Orders Table Created");
-		} catch (SQLException ex) {
-			System.err.println("SQLException: " + ex.getMessage());
-		}
-
-	}
-
-	/*
-	 * Employee_ID Name 6323 Hemanth 5768 Bob 1234 Shawn 5678 Michaels
-	 */
-	public static void insertEmployees() {
-		Connection con = getConnection();
-
-		String insertString1, insertString2, insertString3, insertString4;
-		insertString1 = "insert into Employees values(6323, 'Hemanth')";
-		insertString2 = "insert into Employees values(5768, 'Bob')";
-		insertString3 = "insert into Employees values(1234, 'Shawn')";
-		insertString4 = "insert into Employees values(5678, 'Michaels')";
-
-		try {
-			stmt = con.createStatement();
-			stmt.executeUpdate(insertString1);
-			stmt.executeUpdate(insertString2);
-			stmt.executeUpdate(insertString3);
-			stmt.executeUpdate(insertString4);
+			stmt.executeUpdate(insertString);
 
 			stmt.close();
 			con.close();
 			JOptionPane.showMessageDialog(null,
-					"Data Inserted into Employees Table");
+					"Data Inserted into Users Table");
 		} catch (SQLException ex) {
 			System.err.println("SQLException: " + ex.getMessage());
 		}
 
 	}
 
-	/*
-	 * Prod_ID ProductName Employee_ID 543 Belt 6323 432 Bottle 1234 876 Ring
-	 * 5678
-	 */
-	public static void insertOrders() {
-		Connection con = getConnection();
-
-		String insertString1, insertString2, insertString3;
-		insertString1 = "insert into Orders values(543, 'Belt', 6323)";
-		insertString2 = "insert into Orders values(432, 'Bottle', 1234)";
-		insertString3 = "insert into Orders values(876, 'Ring', 5678)";
-
-		try {
-			stmt = con.createStatement();
-			stmt.executeUpdate(insertString1);
-			stmt.executeUpdate(insertString2);
-			stmt.executeUpdate(insertString3);
-
-			stmt.close();
-			con.close();
-			JOptionPane.showMessageDialog(null,
-					"Data Inserted into Orders Table");
-		} catch (SQLException ex) {
-			System.err.println("SQLException: " + ex.getMessage());
-		}
-
-	}
-
-	public static void retrieveEmployees() {
+	public static void retrieveUsers() {
 		Connection con = getConnection();
 		String result = null;
 		String selectString;
-		selectString = "select * from Employees";
-		result = "ID || Name\n";
+		selectString = "select * from Users";
+		result = "ID || Name || LastName || Date_Birth \n\n";
 		try {
 			stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery(selectString);
 			while (rs.next()) {
 				int id = rs.getInt("ID");
 				String name = rs.getString("Name");
-				result += id + " || " + name + "\n";
+				String lastName = rs.getString("LastName");
+				String date = rs.getString("Date_Birth");
+				result += id + " || " + name + " || " + lastName + " || " + date + "\n";
 			}
 			stmt.close();
 			con.close();
@@ -226,109 +147,20 @@ public class JDBCProgram {
 
 	}
 
-	public static void retrieveOrders() {
+	public static void retrieveUsersWithStatement(){
 		Connection con = getConnection();
 		String result = null;
 		String selectString;
-		selectString = "select * from Orders";
-		result = "ID || ProductName || Employee_ID\n";
-		try {
-			stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery(selectString);
-			while (rs.next()) {
-				int pr_id = rs.getInt("ID");
-				String prodName = rs.getString("ProductName");
-				int id = rs.getInt("Employee_ID");
-				result += pr_id + " || " + prodName + " || " + id + "\n";
-			}
-			stmt.close();
-			con.close();
-			JOptionPane.showMessageDialog(null, result);
-		} catch (SQLException ex) {
-			System.err.println("SQLException: " + ex.getMessage());
-		}
-
-	}
-
-	public static void updateEmployees(){
-		Connection con = getConnection();
-
-		String updateString1;
-		updateString1 = "update Employees set name = 'hemanthbalaji' where id = 6323";
-
-		try {
-			stmt = con.createStatement();
-	   		stmt.executeUpdate(updateString1);
-
-			stmt.close();
-			con.close();
-
-		} catch(SQLException ex) {
-			System.err.println("SQLException: " + ex.getMessage());
-		}
-	JOptionPane.showMessageDialog(null,"Data Updated into Employees Table");
-	}
-
-	public static void updateEmployeesPrepared() {
-		Connection con = getConnection();
-		// create prepared statement
-		try {
-			pstmt = con
-					.prepareStatement("update Employees set name = ? where Id  = ?");
-			pstmt.setString(1, "hemanthbob"); // Note index starts with 1
-			pstmt.setInt(2, 6323);
-
-			pstmt.executeUpdate();
-
-			pstmt.close();
-			con.close();
-
-		} catch (SQLException ex) {
-			System.err.println("SQLException: " + ex.getMessage());
-		}
-		JOptionPane
-				.showMessageDialog(null, "Data Updated into Employees Table");
-	}
-
-	public static void updateOrdersPrepared() {
-
-		int[] productIds = { 543, 432, 876 };
-		String[] productNames = { "cds", "dvds", "Espresso" };
-		int len = productNames.length;
-
-		Connection con = getConnection();
-
-		try {
-			pstmt = con
-					.prepareStatement("update Orders set productname = ? where Id  = ?");
-			for (int i = 0; i < len; i++) {
-				pstmt.setInt(2, productIds[i]);
-				pstmt.setString(1, productNames[i]);
-				pstmt.executeUpdate();
-			}
-
-			pstmt.close();
-			con.close();
-
-		} catch (SQLException ex) {
-			System.err.println("SQLException: " + ex.getMessage());
-		}
-		JOptionPane.showMessageDialog(null, "Data Updated into Orders Table");
-	}
-
-	public static void dynamicQuery(){
-		Connection con = getConnection();
-		String result = null;
-		String selectString;
-		selectString = "select Employees.name from Employees, Orders where productname = 'cds' " +
-			"and Employees.id = Orders.employee_id ";
-	    result ="Name ";
+		String n1 = JOptionPane.showInputDialog(null, "Insert Name");
+		selectString = "select Name, LastName from Users where Name = '"+n1+"' ";
+	    result ="Name || LastName \n\n";
 		try {
 			stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery(selectString);
 			while (rs.next()) {
 			    String name = rs.getString("Name");
-			    result+=name+", ";
+			    String lastName = rs.getString("LastName");
+			    result+=name+" || "+lastName+ "\n";
 			}
 			stmt.close();
 			con.close();
